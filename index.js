d950a29538a1043e9577025fd2824c2e87cd0557
Crazelu <eberelucky64@gmail.com>
2022-04-11 01:32:53 +0100

    fix: adds error handler

--- index.js
+++ index.js
@@ -7,9 +7,8 @@ dotenv.config();
 const app = express();
 
 app.get('/location/:ip', (req, res) => {
-    satelize.satelize({ip:req.params.ip}, function(err, payload) {
-        console.log(payload);
-        if(err != null) res.send("");
+    satelize.satelize({ip:req.params.ip}, function(_, payload) {
+        if(payload != null) res.send("");
         res.send(payload.country.en);
        });
 });
